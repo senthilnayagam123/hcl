628f814f8bdbced03baf354cdc06ec344a7ef31b
senthilnayagam123 <93111604+senthilnayagam123@users.noreply.github.com>
2021-10-25 10:02:42 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package com.org.exception;
+
+public class Student {
+String name ; 
+int age ;
+int marks;
+public Student(String name, int age, int marks) {
+	super();
+	this.name = name;
+	this.age = age;
+	this.marks = marks;
+}
+}
